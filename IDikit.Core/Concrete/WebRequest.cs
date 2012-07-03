80cf7fdc04cf1a105cf6fbc88dd2339abf0efac2
stanleyalston <stanleyalston@hotmail.com>
2012-07-03 14:23:04 +0800

    Initial commit

---
+++ IDikit.Core/Concrete/WebRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace IDikit.Core.Concrete
+{
+    public class WebRequest
+    {
+        public string GetTitle(string url)
+        {
+            return "asdf";
+        }
+    }
+}
---
+++ IDikit.Core/Model/Bookmark.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IDikit.Core.Model
+{
+    public class Bookmark
+    {
+        public int Id { get; set; }
+        public string Link { get; set; } 
+        public string Title { get; set; }        
+    }
+}
